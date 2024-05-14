b97343c569d3a6b7c430995f40f8bda786c2e8d6
emon00007 <skemon152@gmail.com>
2024-05-14 11:59:25 +0600

    food details completed

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 
 
@@ -27,13 +27,43 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
+const foodCollection = client.db('foodSharePlus').collection('foodSharePlusSarver');
+
+
+
+app.post('/foodSharePlusSarver',async(req,res)=>{
+    const newFoodSharePlusSarver =req.body 
+    console.log(newFoodSharePlusSarver)
+    const result =await foodCollection.insertOne(newFoodSharePlusSarver)
+    res.send(result)
+})
+
+
+
+app.get('/foodSharePlusSarver',async(req,res)=>{
+    const cursor= foodCollection.find();
+    const result = await cursor.toArray();
+    res.send(result)
+})
+
+
+app.get('/FoodDetails/:id',async(req,res)=>{
+    console.log(req.params.id)
+    const result = await foodCollection.findOne({_id: new ObjectId(req.params.id)})
+    res.send(result)
+})
+
+
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
